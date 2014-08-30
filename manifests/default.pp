319573b0281751ef2c11d2b9e045da74ce26fda4
narkisr <narkisr@gmail.com>
2014-08-30 22:39:42 +0300

    adding nodes

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,17 +1,23 @@
 group{ 'puppet': ensure  => present }
 
+$nodes = {
+    '1' => {name => 'foo' , address => 'foo:1234'},
+    '2' => {name => 'bar', address   => 'bar:22000'}
+}
+
 node 'freebsd.local' {
 
   $repos = {
     appliances  => {
       directory => '/home/vagrant/Syncthing',
       ro        => false,
-      nodes     => []
+      nodes     => ['1', '2']
     }
   }
 
   class{'backup::syncthing':
-    repos => $repos
+    repos => $repos,
+    nodes => $nodes
   }
 }
 
@@ -21,11 +27,12 @@ node 'ubuntu.local' {
     appliances  => {
       directory => '/home/vagrant/Syncthing',
       ro        => false,
-      nodes     => []
+      nodes     => ['1', '2']
     }
   }
 
   class{'backup::syncthing':
-    repos => $repos
+    repos => $repos,
+    nodes => $nodes
   }
 }
